ac70b4cf7e21d461e19333435c2e64a86eb60d26
Oliver Metz-Strauss <metzstro@mcmaster.ca>
2026-03-03 13:26:49 -0500

    fixed up precursor golem calculator, should work now

--- precursor_golem_copy_calculator/PrecursorGolem.py
+++ precursor_golem_copy_calculator/PrecursorGolem.py
@@ -2,19 +2,17 @@
 # Returns the number of precursor golems and non-precursor golems 
 # after casting a copy spell, including the originals.
 
-# This is wrong btw
-def precursor_golem(precursors: int, golems: int, copies: int = 1) -> tuple[int, int]:
-    if golems == 0:
-        new_precursors = copies + (copies * precursors) * (precursors - 1)
-        new_golems = new_precursors * 2
+# DO NOT INCLUDE THE ORIGINAL TARGET OF THE COPY SPELL IN THE PARAMETER COUNT
+def copy_precursor_golem(precursors: int, golems: int, copies: int, precursor_triggers: int) -> tuple[int, int]:
+    new_precursors = precursors * copies
+    new_golems = (new_precursors * 2) + (copies * golems)
+    if precursor_triggers == 0:
+        # Add back in the original (assumed non-precursor) golem + copies from the original spell
+        return (precursors, golems + 1 + copies)
     else:
-        # Assuming you target a non-precursor with the copy spell
-        new_precursors = copies * precursors * precursors
-        new_golems = new_precursors * 2 + copies + (copies * precursors) * (golems - 1)
-    if copies == 1:
-        return (precursors + new_precursors, golems + new_golems)
-    else:
-        return precursor_golem(precursors + new_precursors, golems + new_golems, copies - 1)
+        return copy_precursor_golem(precursors + new_precursors, golems + new_golems, copies, precursor_triggers - 1)
 
-print(precursor_golem(1, 2, 5))
-print(precursor_golem(6, 22, 5))
+first_rite_precursors, first_rite_golems = copy_precursor_golem(1,1,5,1)
+print(first_rite_precursors, first_rite_golems)
+second_rite_precursors, second_rite_golems = copy_precursor_golem(6, 21, 5, 6)
+print(second_rite_precursors, second_rite_golems)
